2e61f778ad4fce0092145de2bf559901d7453a1a
Evryon <fireden70@gmail.com>
2022-09-05 23:36:20 +0200

    added relu activation

--- main.py
+++ main.py
@@ -12,18 +12,25 @@ np.random.seed(0)
 class Layer:
     def __init__(self, n_inputs, n_neurons):
         self.output = None
-        self.weights = 0.2 * np.random.randn(n_inputs, n_neurons)
+        self.weights = np.random.randn(n_inputs, n_neurons) * 0.1
         self.biases = np.zeros((1, n_neurons))
 
     def advance(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
 
+class Activation:
+    def __init__(self):
+        self.output = None
+
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
 layer1 = Layer(3, 5)
 layer2 = Layer(5, 2)
 
 layer1.advance(X)
-layer2.advance(layer1.output)
-
-print("L1: {\n", layer1.output, "\n{")
-print("L2: {\n", layer2.output, "\n}")
+activation1 = Activation()
+activation1.forward(layer1.output)
+print(activation1.output)
