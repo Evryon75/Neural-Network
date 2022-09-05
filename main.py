2ad9dd157217b8768e163a1f7224d5a078033b7d
Evryon <fireden70@gmail.com>
2022-09-05 22:44:14 +0200

    Made layers

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import numpy as np
 
-class Layer:
-    def __init__(self):
-        pass
+X = [
+    [1.32, 2.12, 1.5],
+    [-2.1, -0.2, 4.1],
+    [3.63, 2.12, -3.3]
+]
 
-    def forward(self):
-        pass
+np.random.seed(0)
+
+
+class Layer:
+    def __init__(self, n_inputs, n_neurons):
+        self.output = None
+        self.weights = 0.2 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def advance(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+layer1 = Layer(3, 5)
+layer2 = Layer(5, 2)
+
+layer1.advance(X)
+layer2.advance(layer1.output)
+
+print("L1: {\n", layer1.output, "\n{")
+print("L2: {\n", layer2.output, "\n}")
